7bcca3210b135b9820411044f85057f56719c823
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-02-23 12:28:59 +0500

    bir

---
+++ index.js
@@ -0,0 +1,211 @@
+//let a;
+//
+//console.log(a)
+//let abdulla;
+//console.log(abdulla)
+//console.log('Let me explain you "flex"')
+//console.log("Let me explain  you 'flex'")
+//let text = ('12 university', 'I have endtered');
+//let date = ('this two-month period')
+//console.log(text, date)
+//let a=undefined
+//console.log(a+12)
+//let text=(`my friend`)
+//console.log(`hello ${text}`);
+//let a = 1, b=1;
+//let a = 2;
+//let x = 1 + (a *= 2);
+//console.log(x)
+//let c = ++a;
+//let d = b++;
+//console.log(c);
+//console.log(d);
+//let name = "Abdulahad"
+//let text;
+//console.log(text);
+//const text = me;
+//console.log(text);
+//const text = she;
+//console.log(text);
+
+//let num = 1, a = 139802387432809323n, b = 'he';
+//console.log(num, a, b);
+
+//let a="welcome to \n my website"
+//console.log(a);
+
+//let a=b=c=3;
+//console.log(a / b);
+
+//let beg='Hitherto, ', sub='I ', verb='have entered ', num=12, place=' universities. ', and='And ', verbs='earned ', big=654000n + 14000n, sign='$.';
+//console.log(beg + sub + verb + num + place + and + verbs + big + sign);
+
+//let a='2';
+//console.log(typeof a);
+
+//let a=28, b=7/3;
+//console.log(a / b);
+
+//let a="welcome to \n \"webbrain\"";
+//console.log(a);
+
+//let a=' 12 universities';
+//console.log('I have entered' + a);
+
+//let a;
+//let b=null;
+//console.log(a, b);
+
+//let a=1, b=2, c=3;
+//console.log(a + b == c);
+//let as=1, bs=2, cs=3;
+//console.log(as + bs === cs);
+//console.log(a - b === c);
+
+//let a;
+//let b=null;
+//console.log(a === b);
+
+//let b;
+//console.log(b + 1)
+
+//let c=null;
+//console.log(c + 1);
+
+//let a;
+//console.log(null + a);
+
+//let big=134932847294n;
+//console.log(null + big);
+
+//let c='big';
+//console.log(null + c);
+
+//let c=null;
+//console.log(null + c);
+
+//let c;
+//console.log(null + c);
+
+//let c;
+//console.log(c + 1);
+
+//let c;
+//let b;
+//console.log(c / b);
+
+//let b=null
+//console.log([b] === [b]);
+
+//let arr = [`I am `, 17, ` years old`]
+//console.log(arr);
+
+//let arr = [`He is`, 16, `years old`];
+//console.log(arr[1.5]);
+
+//let obj = {name:`I am interstedi in javasript`, number:17, id:`me`};
+//console.log(obj);
+
+//let obj = {name:`I am interstedi in javasript`, number:17, id:`me`};
+//let c=[`me`, 17, `cool`];
+//function test(){
+//    console.log(c[0])
+//    console.log(obj.name)
+//};
+//test();
+
+
+//let c=(`she`, `he`, `me`)
+//console.log(`the` + c);
+
+//let c=[17, 'me'];
+//let o={name:'me', age:17};
+//function test(){
+//    console.log(c[1]);
+//    console.log(o.age);
+//}
+//test();
+
+//let c=`me`;
+//console.log(isNaN(c + 1234948498742n));
+
+//string
+//number
+
+//let const var
+//let var
+
+//let a=`Abdulahad`;
+//console.log(`My name is ${a}`);
+
+//let a;
+//console.log(a);
+//let var const
+//=======================================================================================
+//string
+
+//'', "", ``
+
+//number 1)number 1, 2, 3    10/98 
+   //    2)NaN= Not a Number
+   //    3)infinite o'zgarmas
+
+
+
+//NaN=raqam bolmagan narsalar bilan raqamlar ustida arifmetik amal olib borish
+//==================================================================
+
+//let d='34897358934598375n';
+//let c='1348745987345635873n';
+//console.log(d + c);
+//===============================================================
+
+//let c=null;
+//console.log(typeof c);
+
+//===============================================================
+
+
+
+
+
+let a=`me`;
+
+console.log(`she` + a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
